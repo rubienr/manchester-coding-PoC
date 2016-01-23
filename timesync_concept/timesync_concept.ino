0f734ca87d4d8cad454cffc195eb2f202742e860
Raoul Rubien <rubienr@gmail.com>
2016-01-23 11:40:59 +0100

    time sync now more reliable; data provider sending synchronous after pause

--- timesync_concept/timesync_concept.ino
+++ timesync_concept/timesync_concept.ino
@@ -1,369 +1,247 @@
+/*
+ * simple Manchester decoding implementation
+ * @author raoul rubien
+ *
+ * receives manchester coding
+ * synchronizes and adopts the reception clock to te reception received coding
+ *
+ * 01/2016
+ */
 #include <Arduino.h>
 #include <avr/interrupt.h>
+#include<avr/cpufunc.h>
 
-const uint8_t redLed = 3;
-const uint8_t yellowLed = 5;
+typedef struct InitialCounterSettings {
+    const uint16_t top; // the max value the counter 1 is counting to - OCR1A
+    const uint16_t receptionDelta; // allowed reception deviation of center / top
+    const uint16_t leftOfTop; // minimum reception counter at top
+    const uint16_t rightOfTop; // maximum reception counter at top
+    const uint16_t center; //
+    const uint16_t leftOfCenter; // minimum reception counter at center
+    const uint16_t rightOfCenter; // maximum reception counter at center
+    const uint16_t syncCheck; // when to check if bit was received or transmission terminated
+    const uint8_t prescaler;
+};
 
-const uint8_t leftMotor = 9;
-const uint8_t rightMotor = 10;
-
-const uint8_t compTopOutPin = 7;
-//const uint8_t compCenterOutPin = 6;
-const uint8_t syncOutPin = 6;
-
-const uint8_t joystickXPin = 0;
-const uint8_t joystickYPin = 1;
-
-// pin where to listen for input interrupt/data
-const uint8_t inputSignalPin = 13;
-
-const uint8_t topInaccurateOutPin = 21;
-
-// initial approx. data clock
-const uint16_t pkgApproxClkCounter = 4000; // min 4000 @ prescaler=1, internal osc F_CPU=8000000;
-// top cannot drift below/above pkgApproxClkCounter +/- maxPkgClkDrift
-const uint16_t maxPkgClkDrift = (pkgApproxClkCounter / 4) * 0.9;
-// top cannot drift below minCompareA
-const uint16_t minCompareA = pkgApproxClkCounter - maxPkgClkDrift;
-// top cannot drift above maxCompareA
-const uint16_t maxCompareA = pkgApproxClkCounter + maxPkgClkDrift;
-
-// top of timer-counter
-uint16_t compareA = pkgApproxClkCounter;
-// center of timer counter
-uint16_t compareB = compareA / 2;
-// allowed +/- offset from center when signal is captured
-uint16_t centerDelta = (compareA - compareB) / 2;
-// allowed +/- offset from top when signal is captured
-uint16_t topDelta = centerDelta - 1;
-
-// captures counter on input interrupt
-uint16_t captureCounter = 0;
-
-// timer counter 1 prescaler
-// const uint8_t timerClk = (0 << CS12) | (0 << CS11) | (0 << CS10); // disconnected
-const uint8_t timerClk = (0 << CS12) | (0 << CS11) | (1 << CS10); // clk/1
-// const uint8_t timerClk = (0 << CS12) | (1 << CS11) | (0 << CS10); // clk/8
-//const uint8_t timerClk = (0 << CS12) | (1 << CS11) | (1 << CS10); // clk/64
-// const uint8_t timerClk = (1 << CS12) | (0 << CS11) | (0 << CS10); // clk/256
-// const uint8_t timerClk = (1 << CS12) | (0 << CS11) | (1 << CS10); // clk/1024
-
-// max package length in clocks
-const uint8_t maxPackageClocks = 8;
-
-// clocks since current package receiving has started
-uint16_t packageClock = maxPackageClocks;
-
-// if packageIdleClock > maxPackageIdleClock the package is assumed to has ended
-uint8_t packageIdleClock = 0xFF;
-const uint8_t maxPackageIdleClocks = 1;
-
-// to filter bouncing signals if they happen twice at the same reception window: center/top
-uint8_t centerWindowHandledAtCounter = 0xFF;
-uint8_t topWindowHandledAtCounter = 0xFF;
+const InitialCounterSettings Counter1Settings = { //
+        .top = 500, //
+        .receptionDelta = (Counter1Settings.top / 4) - 10, //
+        .leftOfTop = Counter1Settings.top - Counter1Settings.receptionDelta, //
+        .rightOfTop = Counter1Settings.receptionDelta,  //
+        .center = Counter1Settings.top / 2, //
+        .leftOfCenter = Counter1Settings.center - Counter1Settings.receptionDelta, //
+        .rightOfCenter = Counter1Settings.center + Counter1Settings.receptionDelta, //
+        .syncCheck = Counter1Settings.center / 2, //
+        // const uint8_t prescaler = (0 << CS12) | (0 << CS11) | (0 << CS10), // disconnected
+        .prescaler = (0 << CS12) | (0 << CS11) | (1 << CS10), // clk/1
+        // const uint8_t prescaler = (0 << CS12) | (1 << CS11) | (0 << CS10), // clk/8
+        //const uint8_t prescaler = (0 << CS12) | (1 << CS11) | (1 << CS10), // clk/64
+        // const uint8_t prescaler = (1 << CS12) | (0 << CS11) | (0 << CS10), // clk/256
+        // const uint8_t prescaler = (1 << CS12) | (0 << CS11) | (1 << CS10), // clk/1024
+};
 
 
-// TODO tov occurs before 1st compare int is triggered
-ISR(TIMER1_OVF_vect) {
-    digitalWrite(redLed, HIGH);
-}
+uint8_t isReceiving = 0;
+
+/*
+ * PORTH4 - pin 7 - timer counter 1 output signal pin
+ * PORTH3 - pin 6 - is-package-synced. output signal pin
+ * PINK5 - reception input pin
+ * PORTD0 - pin 21 - TIMER1_COMPA_vect interrupt handling duration - green/3
+ * PORTD1 - pin 20 - PCINT2_vect interrupt handling duration - blue/2
+ * PORTD2 -  pin 19 - received data bit output pin -
+ * PORTD3 - pin 18 - TIMER1_COMPB_vect interrupt handling duration - violet/1
+ * PORTB7 - pin 13 - pin where to listen for input interrupt/data
+ *
+ * */
 
 
+// on timer end interrupt: reset timer
 ISR(TIMER1_COMPA_vect) {
-    TCCR1B = 0;
+    PORTD |= (1 << PORTD0);
 
-    TCNT1H = 0;
-    TCNT1L = 0;
+    TCNT1 = 0;
+    PORTH ^= (1 << PORTH4);
 
-    OCR1AH = (0xFF00 & compareA) >> 8;
-    OCR1AL = 0x00FF & compareA;
-    TCCR1B |= timerClk;
-
-    /*
-    compareB = compareA / 2;
-    OCR1BH = (0xFF00 & compareB) >> 8;
-    OCR1BL = 0x00FF & compareB;
-    */
-    packageClock = (packageClock < 0xFFFF) ? packageClock + 1 : 0xFFFF;
-    packageIdleClock = (packageIdleClock < 0xFF) ? packageIdleClock + 1 : 0xFF;
-
-    /*
-    // too many clocks occurred without receiving bits -> package end
-    if (packageIdleClock > maxPackageIdleClocks) {
-        digitalWrite(syncOutPin, LOW);
-        digitalWrite(redLed, LOW);
-    }
-    */
-
-    // reset de-bouncing marks
-    centerWindowHandledAtCounter = 0xFF;
-    topWindowHandledAtCounter = 0xFF;
-
-    //    digitalWrite(yellowLed, !digitalRead(yellowLed));
-    digitalWrite(compTopOutPin, !digitalRead(compTopOutPin));
-
+    PORTD &= ~(1 << PORTD0);
 }
 
+// on sync check interrupt: update status on whether a bit was received or elpsend bit -> transmission end
+ISR(TIMER1_COMPB_vect) {
+    PORTD |= (1 << PORTD3);
 
-//ISR(TIMER1_COMPB_vect) {
-//    digitalWrite(yellowLed, HIGH);
-//    // digitalWrite(compCenterOutPin, !digitalRead(compCenterOutPin));
-//}
+    if (isReceiving == 0) {
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();
+    } else {
+        --isReceiving;
+    }
 
+    if (isReceiving == 0) {
+        PORTH &= ~(1 << PORTH3); // unset sync bit
+        PORTD &= ~(1 << PORTD2); // unset data out
+    } else {
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();
+        _NOP();_NOP();
+    }
 
-float smoothFactor1 = 0.9; // center
-float smoothFactor2 = 0.9; // top: post interrupt
-float smoothFactor3 = 0.9; // top: pre interrupt
-// on received signal interrupt
+    PORTD &= ~(1 << PORTD3);
+}
+
+// on received signal interrupt: classify, handle reception and synchronize/adopt timer
 ISR(PCINT2_vect) {
-    TCCR1B = 0; // disable counting
-    uint8_t isFallingEdge = (PINK & (1 << 5)) == 0; // falling edge of PCINT21/pin5
+    uint8_t isRisingEdge = (PINK & (1 << PINK5)); // edge on PCINT21 - pin 5
+    PORTD |= (1 << PORTD1);
 
+    uint16_t captureCounter = TCNT1;
 
-    captureCounter = TCNT1L;
-    captureCounter |= (TCNT1H << 8);
-
-    // 1st signal of a package
-    if ((isFallingEdge) // start occurs only on falling edge
-        && ((packageIdleClock > maxPackageIdleClocks) // and package was interrupted
-            || (packageClock > maxPackageClocks) // or package exceeds max length
-        )) {
-
-        // TCCR1B = 0; // disable counting
-        TCNT1H = 0;
-        TCNT1L = 0;
-
-        packageClock = 0;
-        packageIdleClock = 0;
-        // TCCR1B |= timerClk; // enable counting
-
-        digitalWrite(syncOutPin, HIGH);
-        //        digitalWrite(redLed, HIGH);
-        digitalWrite(compTopOutPin, !digitalRead(compTopOutPin));
+    // 1st signal of a package:
+    // + synchronize counter
+    if (isReceiving == 0) { // 4
+        if (isRisingEdge == 0) { // 1
+            TCNT1 = 0; // 4
+            PORTH |= (1 << PORTH4); // clk bit // 5
+            PORTH |= (1 << PORTH3); // set sync bit // 5
+            PORTD &= ~(1 << PORTD2); // unset data out // 2
+        } else { // 2
+            _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
+            _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
+            _NOP();_NOP(); // 2
+        }
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
+        _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP(); // 8
     }
 
-        // receiving signal within a package
-    else if ((centerWindowHandledAtCounter != packageClock) && // de-bouncing
-             (packageIdleClock <= maxPackageIdleClocks) && (packageClock <= maxPackageClocks)) {
-        //        captureCounter = TCNT1L;
-        //        captureCounter |= (TCNT1H << 8);
+    else { // receiving signal of a package // 2
 
-        packageIdleClock = 0;
-        centerDelta = (compareA - compareB) / 2;
-        topDelta = (centerDelta == 0) ? 0 : centerDelta - 1;
+        // if signal occurs approx. at 1/2 of a package clock:
+        // + the flank direction defines a data bit
+        // + approximate new counter value
+        if ((Counter1Settings.leftOfCenter <= captureCounter) && // 6
+            (captureCounter <= Counter1Settings.rightOfCenter)) // 16
+        {
+            OCR1A = (Counter1Settings.top + (Counter1Settings.top + 2 * (captureCounter - Counter1Settings.center))) /
+                    2; // 19
+            uint16_t newB = OCR1A / 4; // 4
+            if (OCR1B < captureCounter) { // 2
+                OCR1B = newB; // 4
+            } else {// 2
+                _NOP();_NOP();_NOP();_NOP(); // 4
+            } // 2
 
-        // if signal occurs approx. at 1/2 of a package clock: the flank direction defines a data bit
-        if (//(centerWindowHandledAtCounter != packageClock) &&
-                ((compareB - centerDelta) <= captureCounter) && (captureCounter <= (compareB + centerDelta))) {
-
-            int16_t offset = 2 * (captureCounter - compareB);
-            packageIdleClock = 0;
-
-            compareA += (offset * smoothFactor1);
-            compareA = (compareA < minCompareA) ? pkgApproxClkCounter : compareA;
-            compareA = (compareA > maxCompareA) ? pkgApproxClkCounter : compareA;
-
-            // TCCR1B = 0;
-            OCR1AH = (0xFF00 & compareA) >> 8;
-            OCR1AL = 0x00FF & compareA;
-            // TCCR1B |= timerClk;
-
-            // remember for which package clock this window was handled - de-bouncing signal
-            centerWindowHandledAtCounter = packageClock;
-            topWindowHandledAtCounter = 0xFF;
-        }
-
-            // if signal occurs approx. at the end of the signal clock
-        else if ((topWindowHandledAtCounter != packageClock) && // de-bouncing
-                 (((compareA - topDelta) <= captureCounter) && (captureCounter <= (compareA + topDelta)) ||
-                  (captureCounter <= topDelta))) {
-
-            // TCCR1B = 0; // disable counting
-
-            // remember for which package clock this window was handled - de-bouncing signal
-            centerWindowHandledAtCounter = 0xFF;
-            topWindowHandledAtCounter = packageClock;
-
-            packageIdleClock = 0;
-
-            // compareA interrupt has occurred previously
-            if (captureCounter <= topDelta) {
-                compareA = compareA + (captureCounter * smoothFactor2);
-            } else // approaching compareA interrupt is skipped by now
-            {
-                compareA = captureCounter + ((captureCounter - compareA) * smoothFactor3);
-                packageClock = (packageClock < 0xFFFF) ? packageClock + 1 : 0xFFFF;
-
-                // reset de-bouncing marks
-                centerWindowHandledAtCounter = 0xFF;
-                topWindowHandledAtCounter = 0xFF;
-
-                digitalWrite(yellowLed, !digitalRead(yellowLed));
-                digitalWrite(compTopOutPin, !digitalRead(compTopOutPin));
+            if (isRisingEdge == 0) { // 2
+                PORTD &= ~(1 << PORTD2); // 4
+            } else { // 2
+                PORTD |= (1 << PORTD2); // 4
             }
+        } // 2
+            // if signal occurs approx. at the end/beginning of a package clock:
+            // + synchronize counter
+        else // 2
+        if ((Counter1Settings.leftOfTop <= captureCounter) || // 8
+            (captureCounter <= Counter1Settings.rightOfTop)) { // 9
+            TCNT1 = 0; // 4
+            PORTH ^= (1 << PORTH4); // 6
+            OCR1B = OCR1A / 4; // 12
+            _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();
+            _NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();_NOP();
+            _NOP();
+        } // 2
+    } // 2
 
-            compareA = (compareA < minCompareA) ? pkgApproxClkCounter : compareA;
-            compareA = (compareA > maxCompareA) ? pkgApproxClkCounter : compareA;
-            compareB = compareA / 2;
+    isReceiving = 2;
+    TIFR1 = (1 << OCF1A) | (1 << OCF1B);
 
-            TCNT1H = 0;
-            TCNT1L = 0;
-
-            OCR1AH = (0xFF00 & compareA) >> 8;
-            OCR1AL = 0x00FF & compareA;
-
-            OCR1BH = (0xFF00 & compareB) >> 8;
-            OCR1BL = 0x00FF & compareB;
-            // TCCR1B |= timerClk; // enable counting
-
-        }
-        // un-classify-able signal
-        //        else {
-        //            Serial.print(" M @ ");
-        //            Serial.print(captureCounter);
-        //
-        //            if (topWindowHandledAtCounter == packageClock) {
-        //                Serial.print(" bouncT");
-        //            }
-        //
-        //            if (centerWindowHandledAtCounter == packageClock) {
-        //                Serial.print(" bouncC ");
-        //            }
-        //
-        //            Serial.print(" B ");
-        //            Serial.print(compareB);
-        //            Serial.print(" A ");
-        //            Serial.print(compareA);
-        //            Serial.print(" p ");
-        //            Serial.print(packageClock);
-        //        }
-    }
-    TIFR1 = 0;  // clear pending ICF1 OCF1C OCF1B OCF1A TOV1 interrupts
-    TCCR1B |= timerClk; // enable counting
+    PORTD &= ~(1 << PORTD1);
 }
 
 
-// the setup routine runs once when you press reset:
 void setup() {
 
-    //    Serial.begin(115200);
-    Serial.begin(19200);
-    Serial.println("\n\nrst");
-    Serial.print("approx. top ");
-    Serial.println(pkgApproxClkCounter);
-    Serial.print("approx. center ");
-    Serial.println(compareB);
-    Serial.print("max pkg drift ");
-    Serial.println(maxPkgClkDrift);
-    Serial.print("min top ");
-    Serial.println(minCompareA);
-    Serial.print("max top  ");
-    Serial.println(maxCompareA);
-
     cli();
-    pinMode(redLed, OUTPUT);
-    digitalWrite(redLed, LOW);
-    pinMode(yellowLed, OUTPUT);
-    digitalWrite(yellowLed, LOW);
 
-    pinMode(leftMotor, OUTPUT);
-    digitalWrite(leftMotor, LOW);
-    pinMode(rightMotor, OUTPUT);
-    digitalWrite(rightMotor, LOW);
+    pinMode(21, OUTPUT);
+    digitalWrite(21, LOW);
 
-    //    pinMode(compCenterOutPin, OUTPUT);
-    //    digitalWrite(compCenterOutPin, LOW);
-    pinMode(compTopOutPin, OUTPUT);
-    digitalWrite(compTopOutPin, LOW);
-    pinMode(syncOutPin, OUTPUT);
-    digitalWrite(syncOutPin, LOW);
+    pinMode(20, OUTPUT);
+    digitalWrite(20, LOW);
 
-    pinMode(topInaccurateOutPin, OUTPUT);
-    digitalWrite(topInaccurateOutPin, LOW);
+    pinMode(19, OUTPUT);
+    digitalWrite(19, LOW);
 
+    pinMode(18, OUTPUT);
+    digitalWrite(18, LOW);
 
-    TCNT1H = 0;
-    TCNT1L = 0;
+    pinMode(13, INPUT);
+    digitalWrite(13, HIGH);
 
-    OCR1AH = (0xFF00 & compareA) >> 8;
-    OCR1AL = 0x00FF & compareA;
-    OCR1BH = (0xFF00 & compareB) >> 8;
-    OCR1BL = 0x00FF & compareB;
+    pinMode(7, OUTPUT);
+    digitalWrite(7, LOW);
 
-    TCCR1A = 0;
+    pinMode(6, OUTPUT);
+    digitalWrite(6, LOW);
+
     // disconnect output compare port operation
-    TCCR1A |= (0 << COM1A1) | (0 << COM1A0) | (0 << COM1B1) | (0 << COM1B0) | (0 << COM1C1) | (0 << COM1C0);
+    TCCR1A = (0 << COM1A1) | (0 << COM1A0) | (0 << COM1B1) | (0 << COM1B0) | (0 << COM1C1) | (0 << COM1C0);
     // wave generator mode: normal
+    TCCR1B = (0 << WGM13) | (0 << WGM12);
     TCCR1A |= (0 << WGM11) | (0 << WGM10);
 
-    TCCR1B = 0;
     // input compare noise canelling: off
     TCCR1B |= (0 << ICNC1) | (0 << ICES1);
-    // wave generator mode: normal
-    TCCR1B |= (0 << WGM13) | (0 << WGM12);
     // counter prescaler: disconnected
     TCCR1B |= (0 << CS12) | (0 << CS11) | (0 << CS10);
 
-    TIMSK1 = 0;
     // output compare interrupt enable
-    TIMSK1 |= (0 << ICIE1) | (0 << OCIE1C) | (0 << OCIE1B) | (1 << OCIE1A) | (0 << TOIE1);
+    TIMSK1 = (0 << ICIE1) | (0 << OCIE1C) | (1 << OCIE1B) | (1 << OCIE1A) | (0 << TOIE1);
 
-    pinMode(inputSignalPin, INPUT);
-    digitalWrite(inputSignalPin, HIGH);
 
     // enable PCINT[23:16] interrupt on port K
     PCICR |= (1 << PCIE2);
-    PCMSK2 = 0;
     // enable interrupt PCINT21 to contribute
     PCMSK2 |= (1 << PCINT21);
 
+
+    TCNT1 = 0;
+    OCR1A = Counter1Settings.top;
+    OCR1B = Counter1Settings.syncCheck;
     sei();
-    TCCR1B |= timerClk; // enable counting
+    TCCR1B |= Counter1Settings.prescaler; // enable counting
 }
 
 
 void loop() {
+    uint8_t verbose = false;
 
-    /*
-    int8_t printCounter = 0;
-    while (1) {
-         int x = analogRead(joystickXPin);
-         if (x >= 700) {
-         counterTopOffset = 1000;
-         }
-         else if (x <= 300) {
-         counterTopOffset = -1000;
-         }
-         else {
-         counterTopOffset = 0;
-         }
-
-         int y = analogRead(joystickYPin);
-         if (y >= 700) {
-         counterCenterOffset = 1000;
-         }
-         else if (y <= 300) {
-         counterCenterOffset = -1000;
-         }
-         else {
-         counterCenterOffset = 0;
-         }
-
-         delay(250);
-         counterCenterOffset = 0;
-         counterTopOffset = 0;
-
-         delay(1000);
-    */
-    /*
-        if ((printCounter++ % 4) == 0) {
-            Serial.print("top ");
-            Serial.print(compareA);
-            Serial.print(" center ");
-            Serial.println(compareB);
+    if (verbose) {
+        Serial.begin(19200);
+        Serial.print("[0 tr ");
+        Serial.print(Counter1Settings.rightOfTop);
+        Serial.print(" cl ");
+        Serial.print(Counter1Settings.leftOfCenter);
+        Serial.print(" c ");
+        Serial.print(Counter1Settings.center);
+        Serial.print(" cr ");
+        Serial.print(Counter1Settings.rightOfCenter);
+        Serial.print(" sc ");
+        Serial.print(Counter1Settings.syncCheck);
+        Serial.print(" tl ");
+        Serial.print(Counter1Settings.leftOfTop);
+        Serial.print(" t ");
+        Serial.print(Counter1Settings.top);
+        Serial.print("] - d ");
+        Serial.println(Counter1Settings.receptionDelta);
+        while (1) {
+            Serial.print("sc ");
+            Serial.print(OCR1B);
+            Serial.print(" top ");
+            Serial.println(Counter1Settings.top);
+            delay(1000);
         }
-        delay(250);
+    } else {
+        while (1);
     }
-    */
 }
 
